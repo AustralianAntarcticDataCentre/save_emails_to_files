a0a1dda5ec8de21859487c078ef6f31d4806b03d
Matt McKellar-Spence <Matt.McKellar-Spence@antarctica.gov.au>
2014-12-10 18:06:49 +1100

    Restructure main script to use with statements

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 
 from imap import EmailCheckError, EmailServer
 from message import CSVEmailParser
-from oracle import connect as db_connect
+from oracle import DatabaseServer
 from settings import (
 	DATABASE_STRING, EMAIL_FROM, EMAIL_SUBJECT_RE, IMAP_PASSWORD, IMAP_SERVER,
 	IMAP_USERNAME
@@ -22,6 +22,10 @@ class VoyageEmailParser(CSVEmailParser):
 
 	required_columns = ('Date/Time', 'LATITUDE', 'LONGITUDE')
 
+	#def __init__(self, database, table_name):
+		#self.database = database
+		#self.table_name = table_name
+
 	#def process_csv_content(self, content):
 		# TODO: Save this content to a CSV file as backup.
 		#file_name = ?
@@ -29,7 +33,7 @@ class VoyageEmailParser(CSVEmailParser):
 			#f.write(content)
 
 		#self.process_csv_rows(StringIO(content))
-		#CSVEmailParser.process_csv_content(self, StringIO(content))
+		#CSVEmailParser.process_csv_content(self, content)
 
 	def process_csv_row(self, row):
 		"""
@@ -51,62 +55,60 @@ class VoyageEmailParser(CSVEmailParser):
 		latitude = float(row['LATITUDE'])
 		longitude = float(row['LONGITUDE'])
 
+		#self.database.create_table(self.table_name)
+
 		# TODO: Remove this test code.
 		print(latitude)
 		raise Exception('Stop here')
 
 
 def main():
-	try:
-		server = EmailServer(IMAP_SERVER, IMAP_USERNAME, IMAP_PASSWORD)
-		server.select_inbox()
-	except EmailCheckError as e:
-		logger.error(e.args[0])
-		raise e
+	#with DatabaseServer(DATABASE_STRING) as database:
+	if True:
+		with EmailServer(IMAP_SERVER, IMAP_USERNAME, IMAP_PASSWORD) as server:
+			server.select_inbox()
 
-	for email_message in server.loop_email_messages():
-		#for header_name, header_value in email_message.items():
+			for email_message in server.loop_email_messages():
+				# parseaddr() splits "From" into name and address.
+				# https://docs.python.org/3/library/email.util.html#email.utils.parseaddr
+				email_from = email.utils.parseaddr(email_message['From'])[1]
 
-		# parsedate_tz() includes the timezone.
-		# https://docs.python.org/3.4/library/email.util.html#email.utils.parsedate
-		# https://docs.python.org/3.4/library/email.util.html#email.utils.parsedate_tz
-		#sent_time = email.utils.parsedate_tz(date)
-		#sent_time = email.utils.parsedate(date)
+				logger.debug('Email is from "%s".', email_from)
 
-		# parseaddr() splits "From" into name and address.
-		# https://docs.python.org/3.4/library/email.util.html#email.utils.parseaddr
-		email_from = email.utils.parseaddr(email_message['From'])[1]
+				# Skip this message if it did not come from the correct sender.
+				if email_from != EMAIL_FROM:
+					logger.warning('Email is not from the correct sender (%s).', email_from)
+					continue
 
-		# Skip this message if it did not come from the correct sender.
-		if email_from != EMAIL_FROM:
-			logger.warning('Email is not from the correct sender (%s).', email_from)
-			continue
+				subject = email_message['Subject']
 
-		subject = email_message['Subject']
+				logger.debug('Email subject is "%s".', subject)
 
-		logger.debug('Email subject is "%s".', subject)
+				match_data = EMAIL_SUBJECT_RE.match(subject)
 
-		match_data = EMAIL_SUBJECT_RE.match(subject)
+				# Skip this message if the subject does not match the format.
+				if match_data is None:
+					logger.warning('Email subject does not match the required format.')
+					continue
 
-		# Skip this message if the subject does not match the format.
-		if match_data is None:
-			logger.debug('Email subject does not match the required format.')
-			continue
+				# Get a dict of the values matched in the regex.
+				match_dict = match_data.groupdict()
 
-		# Get a dict of the values matched in the regex.
-		match_dict = match_data.groupdict()
+				# parsedate_tz() includes the timezone.
+				# https://docs.python.org/3/library/email.util.html#email.utils.parsedate
+				# https://docs.python.org/3/library/email.util.html#email.utils.parsedate_tz
+				#sent_time = email.utils.parsedate_tz(date)
 
-		parser = VoyageEmailParser()
+				# Create the table name from the regex values.
+				table_name = 'V{season_code}{voyage_code}'.format(**match_dict)
 
-		#with db_connect(DATABASE_STRING) as db:
-		if True:
-			# Create the table name from the regex values.
-			parser.table_name = 'V{season_code}{voyage_code}'.format(**match_dict)
+				#parser = VoyageEmailParser(database, table_name)
 
-			#db.create_table(parser.table_name)
-
-			parser.process_message(email_message)
-			break
+				#parser.process_message(email_message)
+				break
+		#except EmailCheckError as e:
+			#logger.error(e.args[0])
+			#raise e
 
 
 if '__main__' == __name__:
