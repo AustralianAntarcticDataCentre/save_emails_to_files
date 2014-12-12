f04d683d44507a53be39a2db54d545d2f2a1361b
Matt McKellar-Spence <Matt.McKellar-Spence@antarctica.gov.au>
2014-12-12 14:50:46 +1100

    Add example settings module

---
+++ settings_example.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+from imap import EmailCheckError, EmailServer
+from postgresql import DatabaseServer
+
+
+CSV_FOLDER = os.getcwd()
+
+# Restrict emails by sender.
+EMAIL_FROM = 'sender@example.com'
+
+# Restrict emails by subject.
+EMAIL_SUBJECT_RE = re.compile(''.join([
+	r'(?P<year>\d{4})',
+	r'(?P<month>\d{2})',
+	r'(?P<day>\d{2})',
+	r'(?P<hour>\d{2})',
+	r'(?P<minute>\d{2})',
+	r'\.csv',
+]))
+
+TABLE_NAME_FORMAT = 'data_{year}{month}'
+
+
+def get_database_client():
+	con = 'my_username/my_password@database.example.com:5432/my_database'
+	return DatabaseServer(con)
+
+
+def get_email_client():
+	return EmailServer('mail.example.com', 'my_username', 'my_password')
